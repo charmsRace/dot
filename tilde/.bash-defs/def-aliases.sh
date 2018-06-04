feb96673aae97d579feb52bed324d57a3ef6efe5
Alex Ruble <alex.ruble@q-free.com>
2018-06-04 01:32:57 -0400

    Consolidate VS Code settings, keybindings, snippets

--- tilde/.bash-defs/def-aliases.sh
+++ tilde/.bash-defs/def-aliases.sh
@@ -1,32 +1,37 @@
 #!/usr/bin/env bash
 # echo "def-aliases.sh entered"
 
-alias _="sudo"
-alias ..="cd .."
-alias ...="cd ../.."
-alias .rl=". ~/.bashrc"
-
-alias sagi="sudo apt-get install"
-
-alias tailf="clear && tail -fn +1"
-alias tailw="clear && tail -fn +1 \"$(ls -t | head -n1)\""
-
-alias g="git" # more shortcuts defined in ~/.gitconfig
-
 # shortcuts to quickly edit configuration files
 alias brc="vim ~/.bashrc"
 alias bdi="vim ${D_DEFS}/index.sh"
 alias bda="vim ${D_DEFS}/def-aliases.sh"
 alias vrc="vim ~/.vimrc"
 
-# for a Jira directory
-alias lsj="ls -agGhp | cut -c18-90"
+# general
+alias _="sudo"
+alias sagi="sudo apt-get install"
+alias .rl=". ~/.bashrc"
+
+alias ..="cd .."
+alias ...="cd ../.."
+
+alias lsd="ls -aghpr --group-directories-first"
+alias lst="ls -aghpt"
+alias ..l="cd ..; ls -agGhp"
+alias lsj="ls -agGhp | cut -c18-90" # for Jira
+
+alias treeg="tree -aI \.git"
+
+alias tailf="clear && tail -fn +1"
+alias tailw="clear && tail -fn +1 \"$(ls -t | head -n1)\""
+
+alias g="git" # more macros defined in ~/.gitconfig
 
 # maven
 alias smvn="mvn -T 1C -e"
 alias smvn-l="mvn -T 1C -e -l ./$(${DATEF[@]}).log"
 
-# for chroots
+# for chromebooks with chroots
 if false; then
     alias crouton="sudo sh ~/Downloads/crouton"
     alias edit-chroot="sudo edit-chroot"
