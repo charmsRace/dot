f1a42ad3e0c41104d81d061346da530584a1846e
Alex Ruble <jaruble@ncsu.edu>
2018-06-10 18:50:50 -0400

    Isolate static files

--- sources/attendum/vim-init.sh
+++ sources/attendum/vim-init.sh
@@ -15,6 +15,5 @@ curl -s $BADWOLF_URL -o ${BADWOLF_DEST}/badwolf.vim
 ### Install Vundle
 declare -r VUNDLE_URL=https://github.com/VundleVim/Vundle.vim.git
 declare -r VUNDLE_DEST=${VIM_DIR}/bundle/Vundle.vim
-sudo apt-get install -qqy git curl
 test ! -d $VUNDLE_DEST && git clone $VUNDLE_URL $VUNDLE_DEST
 vim +PluginInstall +qall
