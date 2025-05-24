6050733b02951df4917a37ce351a1d40b9c12707
Clarity Amaranthine <clarity.amaranthine@sscinc.com>
2025-05-24 11:09:53 -0400

    Sync

--- tilde/.bash-defs/def-aliases.sh
+++ tilde/.bash-defs/def-aliases.sh
@@ -2,15 +2,16 @@
 # echo "def-aliases.sh entered"
 
 # shortcuts to quickly edit configuration files
-alias brc="vim ${D_HOME}/.bashrc"
-alias bdi="vim ${D_DEFS}/index.sh"
-alias bda="vim ${D_DEFS}/def-aliases.sh"
-alias bde="vim ${D_DEFS}/def-envars.sh"
-alias bdp="vim ${D_DEFS}/def-private.sh"
+alias .edit-bashrc="vim ${D_HOME}/.bashrc"
+alias .edit-bash-index="vim ${D_DEFS}/index.sh"
+alias .edit-bash-aliases="vim ${D_DEFS}/def-aliases.sh"
+alias .edit-bash-envars="vim ${D_DEFS}/def-envars.sh"
+alias .edit-bash-private="vim ${D_DEFS}/def-private.sh"
 
-alias gcf="vim ${D_HOME}/.gitconfig"
-alias vrc="vim ${D_HOME}/.vimrc"
-alias hfl="sudoedit /c/WINDOWS/System32/drivers/etc/hosts"
+alias .edit-gitconfig="vim ${D_HOME}/.gitconfig"
+alias .edit-gitignore="vim ${D_HOME}/.gitignore-global"
+alias .edit-vimrc="vim ${D_HOME}/.vimrc"
+alias .edit-hosts="sudoedit /c/WINDOWS/System32/drivers/etc/hosts"
 
 # general
 alias _="sudo"
@@ -36,42 +37,72 @@ alias tree-ws="_treews" # '-' disallowed in function names
 alias tailf="clear && tail -fn +1"
 alias tailw="clear && tail -fn +1 \"$(ls -t | head -n1)\""
 
-alias g="git" # more macros defined in ${D_HOME}/.gitconfig
+# more git macros defined in ${D_HOME}/.gitconfig
+alias .g="git"
+alias g="git"
+alias gi="git"
+alias gt="git"
 
 alias isup="ping 8.8.8.8"
 
 # navigation shortcuts
-alias c-="cd - > /dev/null"
-alias cg="cd !$:t:r"
+alias .c-="cd - > /dev/null"
+alias .cg="cd !$:t:r" # e.g. `git clone ...repo.git && cg
 
 # dotfile tools
 alias .cd.="cd ${D_DOT}"
 alias .cd-defs="cd ${D_DEFS}"
 alias .cd-ahk="cd ${D_AHK}"
 alias .cd-vs="cd ${D_VS}"
+alias .cd-ks="cd ${D_KS}"
 
 alias .code.="code ${D_DOT}/.vscode/dot.code-workspace"
 alias .code-defs="code ${D_DEFS}"
-alias .code-ahk="code ${D_AHK}"
+alias .code-ahk="code ${D_AHK}/.vscode/ahk.code-workspace"
 alias .code-vs="code ${D_VS}"
+alias .code-ks="code ${D_KS}/.vscode/knowledge-sharing.code-workspace"
 .code() {
   code "$(ls ./.vscode/*.code-workspace | head -1)"
 }
+
+alias .dc="git dcode"
 
 alias .cp~="cp -r ~/.{bash{-defs,_profile,rc},conemu,git{config,ignore-global},sharpkeys,vimrc} ${D_SOURCE}"
 alias .cp-vs="cp -r ${D_VS}/{css,snippets,{keybindings,settings}.json} ${D_SOURCE}/.vs"
 
 # JS tools
 alias ts-node="npx ts-node"
-alias rnm="npx remove-node-modules"
+alias .rm-node-modules="npx remove-node-modules"
+alias .rm-node-cache="rm -rf ./node_modules/.cache"
 
 # NPM tools
 alias npm-list-global="npm list g --depth 0"
 alias yarn-link-list="ls -la node_modules | grep ^l"
 
-# Python tools
-alias python2="winpty /c/Python27/python.exe"
-alias py2=python2
-alias py3=python3
+# AHK tools
+.run-ahk() {
+  . ${D_AHK}/run.sh
+}
 
-alias protocol="code ${D_GIT}/protocol_1/protocol.txt"
+# .set-ticket() {
+#   local regex='^[0-9]+$'
+#   if [[ ! $1 =~ $regex ]]; then
+#    echo "Error: Not a number" >&2
+#    return
+#   fi
+
+#   sed -i "s/\(^ *FeatureTicket := \)[[:digit:]]\+$/\1$1/" ${D_AHK}/scripts/private/ticket-interpolation.ahk
+#   .run-ahk
+# }
+
+
+# .set-integration-ticket() {
+#   local regex='^[0-9]+$'
+#   if [[ ! $1 =~ $regex ]]; then
+#    echo "Error: Not a number" >&2
+#    return
+#   fi
+
+#   sed -i "s/\(^ *IntegrationTicket := \)[[:digit:]]\+$/\1$1/" ${D_AHK}/scripts/private/ticket-interpolation.ahk
+#   .run-ahk
+# }
--- tilde/.bash-defs/def-prompt.sh
+++ tilde/.bash-defs/def-prompt.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # echo "def-prompt.sh entered"
 
+# PS1=$'clarity \u2208 \W \u2234 ' # plaintext
 PS1=$'\[\033[01;32m\]clarity\[\033[00m\] \u2208 \[\033[01;34m\]\W\[\033[00m\] \u2234 '
